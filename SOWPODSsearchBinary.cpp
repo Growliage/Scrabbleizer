01f8dc9092da4f4c303171b668a68435e7dd6a6f
dbsa11 <dbsa11@student.aau.dk>
2015-12-07 14:18:48 +0100

    Changed SOWPODS loading

--- SOWPODSsearchBinary.cpp
+++ SOWPODSsearchBinary.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <string>
 #include <iostream>
 #include <vector>
@@ -8,7 +9,7 @@ bool SOWPODSsearch(std::string input){
 
 	//Assume the word doesn't exist in the library
 	bool realWord = false;
-	bool binarySearch(std::vector<std::string> linesN, std::string input);
+	bool binarySearch(std::vector<std::string> lines, std::string input);
 
 	if (input.length() < 2 || input.length() > 15){
 		std::cout << "The word does not conform to the correct length!" << std::endl;
@@ -16,15 +17,26 @@ bool SOWPODSsearch(std::string input){
 	}
 	else {
 		//Set the fileName variable to the amount of letters in the word to ensure it looks in the right .txt file
-		std::string linesN = "lines" + std::to_string(input.length());
+		std::string fileName = "data/" + std::to_string(input.length()) + ".txt";
+		std::ifstream dict("fileName");
+
+		dict.open(fileName);
+
+		//Moving the filestream into a vector
+		std::vector<std::string> lines;
+		for (std::string line; std::getline(dict, line);)
+			lines.push_back(line);
+
+		std::cout << "Dictionary has been loaded." << std::endl;
 
 		//Binary search
 		//if(std::binary_search(lines, input) {
 		//realWord = true;
 		//}
 
-		return(binarySearch(linesN, input));
+		return(binarySearch(lines, input));
 
+		dict.close();
 	}
 		//return(realWord);
 }
